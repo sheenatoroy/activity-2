fe276c8661a33dfc668ddf7acf38d8c21a843122
Abegil Tutor <tutorabegil18@gmail.com>
2024-04-12 20:45:32 +0800

    commit product list

---
+++ src/components/ProductList.vue
@@ -0,0 +1,34 @@
+<!-- ProductList.vue -->
+<template>
+    <div>
+      <div v-for="product in products" :key="product.id">
+        <Product :product="product" @edit="editProduct(product)" @delete="deleteProduct(product.id)"/>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Product from './Product.vue';
+  
+  export default {
+    name: 'ProductList',
+    props: ['products'],
+    components: {
+      Product
+    },
+    methods: {
+      editProduct(product) {
+        this.$emit('editProduct', product);
+      },
+      deleteProduct(productId) {
+        if (confirm("Are you sure you want to delete this product?")) {
+          this.$emit('deleteProduct', productId);
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* Add your CSS styles here */
+  </style>  

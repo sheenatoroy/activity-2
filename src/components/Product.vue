a7ef098f3d084dcc847b86b19815ed2051817551
githubaccount <164310409+nicolevelasco29@users.noreply.github.com>
2024-04-12 20:25:04 +0800

    product.vue code

---
+++ src/components/Product.vue
@@ -0,0 +1,30 @@
+<!-- Product.vue -->
+<template>
+    <div>
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.description }}</p>
+      <p>Price: ${{ product.price }}</p>
+      <button @click="edit">Edit</button>
+      <button @click="deleteProduct">Delete</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Product',
+    props: ['product'],
+    methods: {
+      edit() {
+        this.$emit('edit', this.product);
+      },
+      deleteProduct() {
+        this.$emit('delete', this.product.id);
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* Add your CSS styles here */
+  </style>
+  
